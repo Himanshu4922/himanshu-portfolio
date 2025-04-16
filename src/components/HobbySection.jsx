bfb21917886e48094e164b470183c3c799b596e2
himanshu <bishthimanshu829@gmail.com>
2025-04-17 03:41:10 +0530

    Fix date in ProjectsSection

--- src/components/HobbySection.jsx
+++ src/components/HobbySection.jsx
@@ -6,26 +6,26 @@ function HobbySection() {
   const articles = [
     {
       title: "Creating a React element without any bundler",
-      datePublished: "April 11 2025",
-      time: "5 min read",
+      datePublished: "April 12 2025",
+      time: "6 min read",
       url: "https://www.linkedin.com/posts/himanshu-bisht-900a3b247_root-root-frontend-activity-7316574778268520448-0ZD_?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAAD0zKlABSpXbteAvqVQUuIuLdk2CLtiiXks",
     },
     {
       title: "What is Babel ?",
-      datePublished: "April 11 2025",
+      datePublished: "April 15 2025",
       time: "5 min read",
       url: "https://www.linkedin.com/posts/himanshu-bisht-900a3b247_babel-babelcdn-javascript-activity-7317548216843939842-N7K5?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAAD0zKlABSpXbteAvqVQUuIuLdk2CLtiiXks",
     },
     {
       title: "Why React?",
       datePublished: "April 11 2025",
-      time: "5 min read",
+      time: "6 min read",
       url: "https://www.linkedin.com/posts/himanshu-bisht-900a3b247_javascript-reactjs-frontenddevelopment-activity-7316172838636331009-zZOU?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAAD0zKlABSpXbteAvqVQUuIuLdk2CLtiiXks",
     },
     {
       title: "Using React and ReactDom cdn",
-      datePublished: "April 11 2025",
-      time: "5 min read",
+      datePublished: "April 12 2025",
+      time: "7 min read",
       url: "https://www.linkedin.com/posts/himanshu-bisht-900a3b247_javascript-reactwithoutcra-reactjs-activity-7316561500712669184-5_i7?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAAD0zKlABSpXbteAvqVQUuIuLdk2CLtiiXks",
     },
   ];
